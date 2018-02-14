2929fe67f59c32ba3d9e70d21a888adf7e1a0d5f
Deric Walintukan <deric.walintukan@gmail.com>
2018-02-14 22:44:24 +0700

    Remove all src and related files (#17)
    * Remove all src and related files
    
    * Remove rest of files

--- src/test/api/mock/event_factory.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  createTopic: {
-    result: {
-      txid: '1c4d00e5eb3454578774eb0eb79f3bcc32cb15b4ded9624dc2985236a1668c70',
-      sender: 'qKjn4fStBaAtwGiwueJf9qFxgpbAvf1xAy',
-      hash160: '17e7888aa7412a735f336d2f6d784caefabb6fa3',
-    },
-  },
-};
